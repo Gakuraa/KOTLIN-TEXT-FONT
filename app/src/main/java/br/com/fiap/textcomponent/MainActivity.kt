964939b478febed5adb48c1b4e6a06ae11003060
Gakuraa <gabrieliwakura924@gmail.com>
2024-03-15 14:13:41 -0300

    Adição de fontes e estudo do recurso composable Text

---
+++ app/src/main/java/br/com/fiap/textcomponent/MainActivity.kt
@@ -0,0 +1,75 @@
+package br.com.fiap.textcomponent
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.com.fiap.textcomponent.ui.theme.Kablammo
+import br.com.fiap.textcomponent.ui.theme.TextComponentTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TextComponentTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting() {
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Text(
+            modifier = Modifier.background(Color.Magenta)
+                .border(border = BorderStroke(1.dp, color = Color.Blue))
+                .padding(vertical = 32.dp)
+                .width(200.dp)
+                .align(Alignment.CenterHorizontally),
+            text = "Estudando Programação",
+            fontSize = 12.sp,
+            color = Color(0xFFCAB73C),
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold,
+            letterSpacing = 2.sp,
+            fontFamily = Kablammo
+            
+        )
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun GreetingPreview() {
+    TextComponentTheme {
+        Greeting()
+    }
+}
